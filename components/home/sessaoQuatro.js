0471fa522de3a02e54bbe6bc68be3de0e8b5ad56
junioranheu <junioranheu@gmail.com>
2022-03-08 15:11:13 -0300

    sessaoQuatro.js

--- components/home/sessaoQuatro.js
+++ components/home/sessaoQuatro.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Styles from '../../styles/host.SessaoQuatro.module.css';
+import Styles from '../../styles/host.sessaoQuatro.module.css';
 
 export default function SessaoQuatro() {
     return (
