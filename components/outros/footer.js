ccca96e05d7b58fb8cad704e68ac8d8227d42e52
junioranheu <junioranheu@gmail.com>
2022-03-09 17:22:32 -0300

    início footer.js

--- components/outros/footer.js
+++ components/outros/footer.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Styles from '../../styles/footer.module.css';
 
 export default function Footer() {
@@ -7,41 +8,41 @@ export default function Footer() {
                 <div className={Styles.sessao}>
                     <span className={Styles.tituloSessao}>Atendimento</span>
 
-                    <span>Central de Ajuda</span>
-                    <span>Informações de segurança</span>
-                    <span>Opções de cancelamento</span>
-                    <span>Nossa resposta à pandemia de COVID-19</span>
-                    <span>Apoie pessoas com deficiência</span>
-                    <span>Denuncie um problema do bairro</span>
+                    <Link href='/'><a>Central de Ajuda</a></Link>
+                    <Link href='/'><a>Informações de segurança</a></Link>
+                    <Link href='/'><a>Opções de cancelamento</a></Link>
+                    <Link href='/'><a>Nossa resposta à pandemia de COVID-19</a></Link>
+                    <Link href='/'><a>Apoie pessoas com deficiência</a></Link>
+                    <Link href='/'><a>Denuncie um problema do bairro</a></Link>
                 </div>
 
                 <div className={Styles.sessao}>
                     <span className={Styles.tituloSessao}>Comunidade</span>
 
-                    <span>Airbnb.org: ajuda em desastres</span>
-                    <span>Apoie refugiados afegãos</span>
-                    <span>Combate à discriminação</span>
+                    <Link href='/'><a>Airbnb.org: ajuda em desastres</a></Link>
+                    <Link href='/'><a>Apoie refugiados afegãos</a></Link>
+                    <Link href='/'><a>Combate à discriminação</a></Link>
                 </div>
 
                 <div className={Styles.sessao}>
                     <span className={Styles.tituloSessao}>Hospedagem</span>
 
-                    <span>Experimente hospedar</span>
-                    <span>AirCover: proteção ao hospedar</span>
-                    <span>Explore recursos para hospedar</span>
-                    <span>Visite o fórum da comunidade</span>
-                    <span>Hospede de forma responsável</span>
+                    <Link href='/'><a>Experimente hospedar</a></Link>
+                    <Link href='/'><a>AirCover: proteção ao hospedar</a></Link>
+                    <Link href='/'><a>Explore recursos para hospedar</a></Link>
+                    <Link href='/'><a>Visite o fórum da comunidade</a></Link>
+                    <Link href='/'><a>Hospede de forma responsável</a></Link>
                 </div>
 
                 <div className={Styles.sessao}>
                     <span className={Styles.tituloSessao}>Sobre</span>
 
-                    <span>Newsroom</span>
-                    <span>Descubra os novos recursos</span>
-                    <span>Carta dos nossos fundadores</span>
-                    <span>Carreiras</span>
-                    <span>Investidores</span>
-                    <span>Airbnb Luxe</span>
+                    <Link href='/'><a>Newsroom</a></Link>
+                    <Link href='/'><a>Descubra os novos recursos</a></Link>
+                    <Link href='/'><a>Carta dos nossos fundadores</a></Link>
+                    <Link href='/'><a>Carreiras</a></Link>
+                    <Link href='/'><a>Investidores</a></Link>
+                    <Link href='/'><a>Airbnb Luxe</a></Link>
                 </div>
             </div>
         </footer>
