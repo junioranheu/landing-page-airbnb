83eedebaac0a573bdaf2711f052e3519fe2d0283
junioranheu <junioranheu@gmail.com>
2022-03-08 23:06:33 -0300

    footer.js

--- components/home/sesssaoTres.js
+++ components/home/sesssaoTres.js
@@ -1,12 +1,9 @@
-import React from 'react';
 import Styles from '../../styles/host.sessaoTres.module.css';
 
 export default function SessaoTres() {
     return (
-        <React.Fragment >
-            <div className={Styles.sessaoTres}>
-                Três!
-            </div>
-        </React.Fragment > 
+        <div className={Styles.sessaoTres}>
+            Três!
+        </div>
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import 'animate.css/animate.min.css';
+import React from 'react';
+import Footer from '../components/outros/footer';
 import '../fonts/circular.css';
 import '../fonts/homes/crusellia.css';
 import '../fonts/homes/hybi4.css';
@@ -7,7 +9,12 @@ import '../fonts/homes/saturdaypartygt.css';
 import '../styles/globals.css';
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Component {...pageProps} />
+      <Footer />
+    </React.Fragment>
+  )
 }
 
 export default App
