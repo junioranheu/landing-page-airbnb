20a76317b8ee013cd1704c764e9157db502e0a28
junioranheu <junioranheu@gmail.com>
2022-03-08 22:28:27 -0300

    iniciar sessaoQuatro.js

--- components/home/sessaoQuatro.js
+++ components/home/sessaoQuatro.js
@@ -1,12 +1,44 @@
-import React from 'react';
 import Styles from '../../styles/host.sessaoQuatro.module.css';
 
 export default function SessaoQuatro() {
     return (
-        <React.Fragment >
-            <div className={Styles.sessaoQuatro}>
-                Quatro
+        <div className={Styles.sessaoQuatro}>
+            <div className={Styles.divPrincipal}>
+                <div className={Styles.cardUm}>
+                    <span>
+                        Dúvidas sobre como se tornar um anfitrião?
+                    </span>
+
+                    <span>
+                        Pergunte a um Superhost.
+                    </span>
+
+                    <div className={Styles.divBotao}>
+                        <input className={Styles.botao} type='button' value='Saiba mais' />
+                    </div>
+                </div>
             </div>
-        </React.Fragment >
+
+            <div className={Styles.divPrincipal}>
+                <div className={Styles.cardDois}>
+                    <span>
+                        <span className={Styles.aircoverUm}>air</span>      
+                        <span className={Styles.aircoverDois}>cover</span>
+                    </span>
+
+                    <span>
+                        Proteção ampla e gratuita para
+                    </span>
+
+                    <span>
+                        anfitriões. Somente no Airbnb.
+                    </span>
+
+                    <div className={Styles.divBotao}>
+                        <input className={Styles.botao} type='button' value='Conheça o AirCover' />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
--- pages/host/home.js
+++ pages/host/home.js
@@ -1,14 +1,18 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import SessaoQuatro from '../../components/home/sessaoQuatro';
 import SessaoUm from '../../components/home/sessaoUm';
 import SessaoDois from '../../components/home/sesssaoDois';
 import SessaoTres from '../../components/home/sesssaoTres';
 
 export default function Home() {
+    useEffect(() => {
+        document.title = 'Hospede em sua acomodação no Airbnb';
+    }, [])
+
     const refSessaoDois = useRef(null);
     function handleBotaoSessaoDois() {
         refSessaoDois.current.scrollIntoView();
-    }
+    } 
 
     return (
         <React.Fragment>
