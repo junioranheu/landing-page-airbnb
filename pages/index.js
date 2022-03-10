70850414fdc8d32187b0293b4ae756fe68ba53cd
junioranheu <junioranheu@gmail.com>
2022-03-10 00:07:27 -0300

    ajuste provisório em index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,10 @@ export default function Index() {
     return (
         <div>
             <Link href='/host/home'>
-                <a>/host/home.js</a>
+                <a>
+                    <h1>Projeto em desenvolvimento</h1>
+                    <h3>Clique aqui para ser direcionado à página em construção</h3>
+                </a>
             </Link>
         </div>
     )
