c53dab13a2cad20d00f24b58c324d6558c817787
junioranheu <junioranheu@gmail.com>
2022-03-10 20:49:16 -0300

    sessaoTres.js

--- components/home/sesssaoTres.js
+++ components/home/sesssaoTres.js
@@ -1,38 +1,70 @@
 import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
 import Styles from '../../styles/host.sessaoTres.module.css';
 
 export default function SessaoTres() {
+    const [tipoEspaco, setTipoEspaco] = useState(450);
+    function handleChangeTipoEspaco(e) {
+        setTipoEspaco(e.target.value);
+    }
+
+    const listaHospedes = ['1 hóspede', '2 hóspedes', '3 hóspedes', '4 hóspedes', '5 hóspedes', '6 hóspedes', '7 hóspedes'];
+    const [qtdHospedes, setQtdHospedes] = useState(1);
+    function handleChangeHospedes(e) {
+        setQtdHospedes(e.target.value);
+    }
+
+    const [localizado, setLocalizado] = useState(1.2);
+    function handleChangeLocalizado(e) {
+        setLocalizado(e.target.value);
+    }
+
+    const [dinheiroTotal, setDinheiroTotal] = useState(0);
+    useEffect(() => {
+        // Cálculo para SIMULAR o que é feito no site oficial;
+        const porHospede = 180; // Só para simular :P
+        const porHospedeFinal = porHospede * qtdHospedes;
+        let dinheiro = ((tipoEspaco * localizado) + porHospedeFinal);
+        setDinheiroTotal(dinheiro);
+    }, [tipoEspaco, qtdHospedes, localizado])
+
     return (
         <div className={Styles.sessaoTres}>
             <div>
                 <span className={Styles.titulo}>Qual o tipo do seu espaço?</span>
 
-                <select className={Styles.select}
-                // value={value}
-                // onChange={(e) => {
-                //     setValue(e.target.value);
-                // }}           
-                >
-                    <option value='option-1'>Lugar inteiro</option>
-                    <option value='option-2'>Quarto inteiro</option>
-                    <option value='option-3'>Quarto compartilhado</option>
+                <select className={Styles.select} onChange={(e) => handleChangeTipoEspaco(e)}>
+                    <option value='450'>Lugar inteiro</option>
+                    <option value='275'>Quarto inteiro</option>
+                    <option value='150'>Quarto compartilhado</option>
                 </select>
             </div>
 
             <div>
                 <span className={Styles.titulo}>Quantas pessoas podem se hospedar?</span>
-                <span className={Styles.opcao}>4 hóspedes</span>
+
+                <select className={Styles.select} onChange={(e) => handleChangeHospedes(e)}>
+                    {listaHospedes.map((item, i) => (
+                        <option value={(i + 1)} key={i}>{item}</option>
+                    ))}
+                </select>
             </div>
 
             <div>
                 <span className={Styles.titulo}>Onde está localizado?</span>
-                <span className={Styles.opcao}>Guaratingueta</span>
+
+                <select className={Styles.select} onChange={(e) => handleChangeLocalizado(e)}>
+                    <option value='1.2'>Lorena</option>
+                    <option value='1.4'>Guaratinguetá</option>
+                    <option value='2'>São José dos Campos</option>
+                    <option value='2.5'>São Paulo</option>
+                </select>
             </div>
 
             <div>
-                <span className={Styles.tituloInfo}>Ganhe até R$2.982 por mês*</span>
+                <span className={Styles.tituloInfo}>Ganhe até R${dinheiroTotal} por mês*</span>
                 <Link href='/'><a className={Styles.info}>Clique aqui para ser direcionado à página em construção</a></Link>
             </div>
-        </div>
+        </div >
     )
 }
