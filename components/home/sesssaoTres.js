126c552dfed04349fc4482fcc0fb7b26d21b1269
junioranheu <junioranheu@gmail.com>
2022-03-10 16:30:38 -0300

    next.config.js

--- components/home/sesssaoTres.js
+++ components/home/sesssaoTres.js
@@ -3,7 +3,14 @@ import Styles from '../../styles/host.sessaoTres.module.css';
 export default function SessaoTres() {
     return (
         <div className={Styles.sessaoTres}>
-            Três!
+            Qual o tipo do seu espaço?
+
+            Quantas pessoas podem se hospedar?
+
+            Onde está localizado?
+            Guaratingueta
+            Ganhe até R$2.982 por mês*
+            Como estimamos seu potencial de ganho
         </div>
     )
 }
--- next.config.js
+++ next.config.js
@@ -1,2 +1,7 @@
 const withVideos = require('next-videos'); // Habilitar import de vídeos; https://www.npmjs.com/package/next-videos
-module.exports = withVideos()
+
+module.exports = withVideos(
+    {
+        swcMinify: true // https://nextjs.org/docs/advanced-features/compiler
+    }
+)
