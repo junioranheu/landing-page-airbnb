edf2ea7a6e59d45145490af05e1a119dd6d3c24f
junioranheu <junioranheu@gmail.com>
2022-03-15 12:52:15 -0300

    playsinline='true' disablePictureInPicture='true'

--- components/home/sessao1.js
+++ components/home/sessao1.js
@@ -115,7 +115,7 @@ export default function SessaoUm(props) {
             </div>
 
             <div className={Styles.divDireita}>
-                <video className={Styles.video} autoPlay loop muted ref={refVideo}
+                <video className={Styles.video} autoPlay loop muted playsinline='true' disablePictureInPicture='true' ref={refVideo}
                     aria-label='Chelsea, anfitriã na Filadélfia, sorri ao abrir a porta. Soraya, anfitriã em Mumbai, sorri ao abrir a porta da frente com outra pessoa ao seu lado. Mohamed, anfitrião em Joanesburgo, sorri ao abrir a porta. Maria, anfitriã na Cidade do México, acena com a cabeça e sorri ao carregar uma planta.'>
                     <source src={VideoUm} type='video/mp4' />
                 </video>
