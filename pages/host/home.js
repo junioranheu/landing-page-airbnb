a07dd11d24b69ac4b5ce046c00adba0fe1f3c934
junioranheu <junioranheu@gmail.com>
2022-03-14 23:03:13 -0300

    ajuste em aviso() em home.js

--- pages/host/home.js
+++ pages/host/home.js
@@ -55,7 +55,7 @@ export default function Home() {
         const msg =
             `Olá! ${EmojiAleatorio()}<br/><br/> 
           Essa página foi replicada, sem fins lucrativos, a fim de estudo apenas, utilizando React.js e Next.js, a partir de um projeto real, de uma empresa real.<br/><br/> 
-          Feito por @junioranheu<br/><br/> 
+          Feito por @junioranheu.<br/><br/> 
           Todos os direitos reservados à @airbnb`;
         Aviso.custom(msg, 20000);
     }, []);
